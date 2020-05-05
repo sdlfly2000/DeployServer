ce6f4a4a0d0d663d39669e787303ca6e64be958f
Jay Shi <sdlfly2000@163.com>
2020-05-05 16:26:03 +0800

    Add Action restart service

--- DeployAction.py
+++ DeployAction.py
@@ -5,48 +5,51 @@ import logging
 from enum import IntEnum
 
 BUFF_SIZE = 512
-ZipFile = ""
-ZipFileDestination = ""
+ZipFile = "/home/sdlfly2000/Uploads/netcoreapp3.1.zip"
+ZipFileDestination = "/home/sdlfly2000/Projects/ImageReceiveService/"
 
 class DeployAction(socketserver.BaseRequestHandler):
     def handle(self):
         self.logger = logging.getLogger(name="deployLogger")
-        self.data = bytearray()
+        self.data = []
         chunckSize = 0
         try:
             while True:
                 tData = self.request.recv(BUFF_SIZE)
                 if(chunckSize == 0):
-                    chunckSize = int.from_bytes(tData[0:2])
-                    self.data.append(tData)
-                if (chunckSize == self.data.count):
+                    chunckSize = int.from_bytes(tData[0:2], byteorder='big')
+                    for data in tData:
+                        self.data.append(data)
+                if (chunckSize == len(self.data)):
                     break          
             self.logger.info("Data received: %s" % self.data)
-            action = self._ParseAction(self.data)
+            self._ParseAction(self.data)
 
             # self.request.sendall(self.data.upper())
         except Exception as e:
-            print(str(e))
+            self.logger.info(str(e))
 
     def _ParseAction(self, data):
         actionByte = data[2]
         if(actionByte == Action.UnZipAction):
             self._UnZipFile(ZipFile, ZipFileDestination)
         elif (actionByte == Action.RestartService):
-            self.RestartHostServer()
+            self._RestartHostServer()
         else:
             pass
 
     def _UnZipFile(self, zipFile, destination):
         try:
+            self.logger.info("Unzip ZipFile: %s" % zipFile)
+            self.logger.info("Unzip Destination: %s" % destination)
             if zipfile.is_zipfile(zipFile):
                 with zipfile.ZipFile(zipFile, 'r') as zipf:
                     zipf.extractall(destination)
         except Exception as e:
-            print(str(e))
+            self.logger.info(str(e))
         
-    def RestartHostServer(self):
-        pass
+    def _RestartHostServer(self):
+        os.system("echo %s | sudo -S %s" % ("sdl@1215", "service supervisor restart"))
 
 class Action(IntEnum):
     UnZipAction = 1
--- testClient.py
+++ testClient.py
@@ -2,10 +2,12 @@ import socket
 
 if __name__ == "__main__":
     s = socket.socket()         
-    # host = socket.gethostname() 
-    host = "182.61.37.221"
+    host = socket.gethostname() 
+    # host = "182.61.37.221"
     port = 4001                
     
+    data = [0x00, 0x03, 0x01]
+    
     s.connect((host, port))
-    s.send("Hello".encode())
+    s.send(bytes(data))
     s.close()
